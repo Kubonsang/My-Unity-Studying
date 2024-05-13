b99349799c3df8ef8e223fd5b827722c3d70390a
Kubonsang <165915671+Kubonsang@users.noreply.github.com>
2024-05-13 21:18:50 +0900

    5월 11 ~ 12일
     애니메이션을 설정하는데 있어서, 책과 버전이 다른 것을 고치는 데 시간을 소비했다.
     그리고, 보충 설정으로 애니메이션이 더 자연스럽게 변환되도록 설정했다.
    
     공격 애니메이션은 구현만 해놓았고, 공격 키를 설정하면 된다.

---
+++ Assets/Scripts/CharaAnimation.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class CharaAnimation : MonoBehaviour
+{
+	Animator animator;
+	CharacterStatus status;
+	Vector3 prePosition;
+	bool isDown = false;
+	bool attacked = false;
+
+	
+	public bool IsAttacked()
+	{
+		return attacked;
+	}
+	
+	void StartAttackHit()
+	{
+		Debug.Log ("StartAttackHit");
+	}
+	
+	void EndAttackHit()
+	{
+		Debug.Log ("EndAttackHit");
+	}
+	
+	void EndAttack()
+	{
+		attacked = true;
+	}
+	
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
+		status = GetComponent<CharacterStatus>();
+		
+		prePosition = transform.position;
+	}
+	
+	void Update ()
+	{
+		Vector3 delta_position = transform.position - prePosition;
+		if(delta_position.magnitude / Time.deltaTime > 0.5 )
+		{
+			animator.SetBool("isMoving", true);
+		}
+		else
+		{
+			animator.SetBool("isMoving", false);
+		}
+		
+		if(attacked && !status.attacking)
+		{
+			attacked = false;
+		}
+		animator.SetBool("Attacking", (!attacked && status.attacking));
+		
+		if(!isDown && status.died)
+		{
+			isDown = true;
+			animator.SetTrigger("Down");
+		}
+		
+		prePosition = transform.position;
+	}
+}
--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class CharacterStatus : MonoBehaviour
 {
+
+    //---------- 공격 장에서 사용한다. ----------
+    // 체력.
     public int HP = 100;
     public int MaxHP = 100;
 
+    // 공격력.
     public int Power = 10;
 
-    public GameObject lastAttackTarget;
+    // 마지막에 공격한 대상.
+    public GameObject lastAttackTarget = null;
 
+    //---------- GUI 및 네트워크 장에서 사용한다. ----------
+    // 플레이어 이름.
     public string characterName = "Player";
 
+    //--------- 애니메이션 장에서 사용한다. -----------
+    // 상태.
     public bool attacking = false;
     public bool died = false;
+
 }
